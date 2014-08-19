0b1d61dc0fecf42b40200197c1c33102fbb90e9b
John Gallagher <jgallagher@bignerdranch.com>
2014-08-19 07:19:10 -0700

    Ensure correct lock behavior.
    This commit fixes two (related) problems:
    
    * Due to the semantics of inout stored properties, value types can't
      participate in synchronization of themselves. Therefore, change the
      ReadWriteLock protocol to require a class (disallowing structs).
    
    * Swift doesn't guarantee stable memory addresses for stored properties,
      so instead of using raw Int32 properties for OSAtomic and friends, we
      use an explicit UnsafeMutablePointer (which is manually allocated and
      deallocated and therefore not subject to Swift moving it around behind
      the scenes).
    
    Neither of these were problems in practice (in beta 5 or beta 6) for the
    LockProtected class because it is declared as "final", and optimizations
    with final classes sidestepped both of these problems. However, that
    isn't guaranteed to always be the case. With these changes, the classes
    should work correctly given the semantics of inout properties even in
    the absence of optimizations.

--- Deferred/ReadWriteLock.swift
+++ Deferred/ReadWriteLock.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-public protocol ReadWriteLock {
-    mutating func withReadLock<T>(block: () -> T) -> T
-    mutating func withWriteLock<T>(block: () -> T) -> T
+public protocol ReadWriteLock: class {
+    func withReadLock<T>(block: () -> T) -> T
+    func withWriteLock<T>(block: () -> T) -> T
 }
 
-public struct GCDReadWriteLock: ReadWriteLock {
+public class GCDReadWriteLock: ReadWriteLock {
     private let queue = dispatch_queue_create("GCDReadWriteLock", DISPATCH_QUEUE_CONCURRENT)
 
     public init() {}
@@ -35,53 +35,69 @@ public struct GCDReadWriteLock: ReadWriteLock {
     }
 }
 
-public struct SpinLock: ReadWriteLock {
-    private var lock = OS_SPINLOCK_INIT
+public class SpinLock: ReadWriteLock {
+    private var lock: UnsafeMutablePointer<Int32>
 
-    public init() {}
+    public init() {
+        lock = UnsafeMutablePointer.alloc(1)
+        lock.memory = OS_SPINLOCK_INIT
+    }
 
-    public mutating func withReadLock<T>(block: () -> T) -> T {
-        OSSpinLockLock(&lock)
+    deinit {
+        lock.dealloc(1)
+    }
+
+    public func withReadLock<T>(block: () -> T) -> T {
+        OSSpinLockLock(lock)
         let result = block()
-        OSSpinLockUnlock(&lock)
+        OSSpinLockUnlock(lock)
         return result
     }
 
-    public mutating func withWriteLock<T>(block: () -> T) -> T {
-        OSSpinLockLock(&lock)
+    public func withWriteLock<T>(block: () -> T) -> T {
+        OSSpinLockLock(lock)
         let result = block()
-        OSSpinLockUnlock(&lock)
+        OSSpinLockUnlock(lock)
         return result
     }
 }
 
 /// Test comment 2
-public struct CASSpinLock: ReadWriteLock {
-    private static let WRITER_BIT: Int32         = 0x40000000
-    private static let WRITER_WAITING_BIT: Int32 = 0x20000000
-    private static let MASK_WRITER_BITS          = WRITER_BIT | WRITER_WAITING_BIT
-    private static let MASK_READER_BITS          = ~MASK_WRITER_BITS
+public class CASSpinLock: ReadWriteLock {
+    private struct Masks {
+        static let WRITER_BIT: Int32         = 0x40000000
+        static let WRITER_WAITING_BIT: Int32 = 0x20000000
+        static let MASK_WRITER_BITS          = WRITER_BIT | WRITER_WAITING_BIT
+        static let MASK_READER_BITS          = ~MASK_WRITER_BITS
+    }
 
-    private var _state: Int32 = 0
+    private var _state: UnsafeMutablePointer<Int32>
 
-    public init() {}
+    public init() {
+        _state = UnsafeMutablePointer.alloc(1)
+        _state.memory = 0
+    }
 
-    public mutating func withWriteLock<T>(block: () -> T) -> T {
+    deinit {
+        _state.dealloc(1)
+    }
+
+    public func withWriteLock<T>(block: () -> T) -> T {
         // spin until we acquire write lock
         do {
-            let state = _state
+            let state = _state.memory
 
             // if there are no readers and no one holds the write lock, try to grab the write lock immediately
-            if (state == 0 || state == CASSpinLock.WRITER_WAITING_BIT) &&
-                OSAtomicCompareAndSwap32Barrier(state, CASSpinLock.WRITER_BIT, &_state) {
+            if (state == 0 || state == Masks.WRITER_WAITING_BIT) &&
+                OSAtomicCompareAndSwap32Barrier(state, Masks.WRITER_BIT, _state) {
                     break
             }
 
             // If we get here, someone is reading or writing. Set the WRITER_WAITING_BIT if
             // it isn't already to block any new readers, then wait a bit before
             // trying again. Ignore CAS failure - we'll just try again next iteration
-            if state & CASSpinLock.WRITER_WAITING_BIT == 0 {
-                OSAtomicCompareAndSwap32Barrier(state, state | CASSpinLock.WRITER_WAITING_BIT, &_state)
+            if state & Masks.WRITER_WAITING_BIT == 0 {
+                OSAtomicCompareAndSwap32Barrier(state, state | Masks.WRITER_WAITING_BIT, _state)
             }
         } while true
 
@@ -90,11 +106,11 @@ public struct CASSpinLock: ReadWriteLock {
 
         // unlock
         do {
-            let state = _state
+            let state = _state.memory
 
             // clear everything except (possibly) WRITER_WAITING_BIT, which will only be set
             // if another writer is already here and waiting (which will keep out readers)
-            if OSAtomicCompareAndSwap32Barrier(state, state & CASSpinLock.WRITER_WAITING_BIT, &_state) {
+            if OSAtomicCompareAndSwap32Barrier(state, state & Masks.WRITER_WAITING_BIT, _state) {
                 break
             }
         } while true
@@ -102,14 +118,14 @@ public struct CASSpinLock: ReadWriteLock {
         return result
     }
 
-    public mutating func withReadLock<T>(block: () -> T) -> T {
+    public func withReadLock<T>(block: () -> T) -> T {
         // spin until we acquire read lock
         do {
-            let state = _state
+            let state = _state.memory
 
             // if there is no writer and no writer waiting, try to increment reader count
-            if (state & CASSpinLock.MASK_WRITER_BITS) == 0 &&
-                OSAtomicCompareAndSwap32Barrier(state, state + 1, &_state) {
+            if (state & Masks.MASK_WRITER_BITS) == 0 &&
+                OSAtomicCompareAndSwap32Barrier(state, state + 1, _state) {
                     break
             }
         } while true
@@ -119,16 +135,16 @@ public struct CASSpinLock: ReadWriteLock {
 
         // decrement reader count
         do {
-            let state = _state
+            let state = _state.memory
 
             // sanity check that we have a positive reader count before decrementing it
-            assert((state & CASSpinLock.MASK_READER_BITS) > 0, "unlocking read lock - invalid reader count")
+            assert((state & Masks.MASK_READER_BITS) > 0, "unlocking read lock - invalid reader count")
 
             // desired new state: 1 fewer reader, preserving whether or not there is a writer waiting
-            let newState = ((state & CASSpinLock.MASK_READER_BITS) - 1) |
-                (state & CASSpinLock.WRITER_WAITING_BIT)
+            let newState = ((state & Masks.MASK_READER_BITS) - 1) |
+                (state & Masks.WRITER_WAITING_BIT)
 
-            if OSAtomicCompareAndSwap32Barrier(state, newState, &_state) {
+            if OSAtomicCompareAndSwap32Barrier(state, newState, _state) {
                 break
             }
         } while true

2830f35d2083e513a4a7999e488e678508e72bda
Zachary Waldowski <zach@waldowski.me>
2015-08-23 21:25:17 -0400

    Documentation for the members of Deferred

--- Deferred/Deferred.swift
+++ Deferred/Deferred.swift
@@ -12,31 +12,50 @@ private var DeferredDefaultQueue: dispatch_queue_t {
     return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
 }
 
+/// A deferred is a value that may become determined (or "filled") at some point
+/// in the future. Once a deferred value is determined, it cannot change.
 public struct Deferred<Value> {
     typealias UponBlock = (dispatch_queue_t, Value -> ())
     private typealias Protected = (protected: Value?, uponBlocks: [UponBlock])
-
+    
     private var protected: LockProtected<Protected>
-
+    
     private init(_ value: Value?) {
         protected = LockProtected(item: (value, []))
     }
-
-    // Initialize an unfilled Deferred
+    
+    /// Initialize an unfilled Deferred.
     public init() {
         self.init(nil)
     }
-
-    // Initialize a filled Deferred with the given value
+    
+    /// Initialize a filled Deferred with the given value.
     public init(value: Value) {
         self.init(value)
     }
-
-    // Check whether or not the receiver is filled
+    
+    /// Check whether or not the receiver is filled.
     public var isFilled: Bool {
         return protected.withReadLock { $0.protected != nil }
     }
     
+    /// Determines the deferred value with a given result.
+    ///
+    /// Filling a deferred value should usually be attempted only once, and by
+    /// default filling will trap upon improper usage.
+    ///
+    /// * In playgrounds and unoptimized builds (the default for a "Debug"
+    ///   configuration), program execution will be stopped at the caller in
+    ///   a debuggable state.
+    /// * In -O builds (the default for a "Release" configuration), program
+    ///   execution will stop.
+    /// * In -Ounchecked builds, the programming error is assumed to not exist.
+    ///
+    /// If your deferred requires multiple potential fillers to race, you may
+    /// disable the precondition.
+    ///
+    /// :param: value The resolved value of the deferred.
+    /// :param: assertIfFilled If `false`, race checking is disabled.
     public func fill(value: Value, assertIfFilled: Bool = true, file: StaticString = __FILE__, line: UWord = __LINE__) {
         let (filledValue, blocks) = protected.withWriteLock { data -> (Value, [UponBlock]) in
             if assertIfFilled {
@@ -53,11 +72,25 @@ public struct Deferred<Value> {
             dispatch_async(queue) { block(filledValue) }
         }
     }
-
+    
+    /**
+    Checks for and returns a determined value.
+    
+    :returns: The determined value, if already filled, or `nil`.
+    */
     public func peek() -> Value? {
         return protected.withReadLock { $0.protected }
     }
-
+    
+    /**
+    Call some function once the value is determined.
+    
+    If the value is already determined, the function will be submitted to the
+    queue immediately. An `upon` call is alreadys executed asnychronously.
+    
+    :param: queue A dispatch queue for executing the given function on.
+    :param: function A function that uses the determined value.
+    */
     public func upon(_ queue: dispatch_queue_t = DeferredDefaultQueue, function: Value -> ()) {
         let maybeValue: Value? = protected.withWriteLock{ data in
             if data.protected == nil {
@@ -72,6 +105,17 @@ public struct Deferred<Value> {
 }
 
 extension Deferred {
+    /**
+    Waits for the value to become determined, then returns it.
+
+    This is equivalent to unwrapping the value of calling `wait(.Forever)`, but
+    may be more efficient.
+
+    This getter will unnecessarily block execution. It might be useful for
+    testing, but otherwise it should be strictly avoided.
+
+    :returns: The determined value.
+    */
     public var value: Value {
         // fast path - return if already filled
         if let v = peek() {
@@ -89,6 +133,17 @@ extension Deferred {
 }
 
 extension Deferred {
+    /**
+    Begins another asynchronous operation with the deferred value once it
+    becomes determined.
+
+    :param: queue A dispatch queue for starting the new operation on.
+    :param: transform A function to start a new deferred given the recieving
+    value.
+
+    :returns: A new deferred value that is determined once the recieving
+    deferred is determined by beginning some new operation using that value.
+    **/
     public func flatMap<NewValue>(upon queue: dispatch_queue_t = DeferredDefaultQueue, transform: Value -> Deferred<NewValue>) -> Deferred<NewValue> {
         let d = Deferred<NewValue>()
         upon(queue) {
@@ -99,6 +154,14 @@ extension Deferred {
         return d
     }
 
+    /**
+    Transforms the deferred value once it becomes determined.
+
+    :param: queue A dispatch queue for executing the transform on.
+    :param: transform A function to create something using the deferred value.
+    :returns: A new deferred value that is determined once the recieving
+    deferred is determined.
+    **/
     public func map<NewValue>(upon queue: dispatch_queue_t = DeferredDefaultQueue, transform: Value -> NewValue) -> Deferred<NewValue> {
         let d = Deferred<NewValue>()
         upon(queue) {
@@ -109,11 +172,26 @@ extension Deferred {
 }
 
 extension Deferred {
+    /**
+    Composes the recieving value with another.
+
+    :param: other Any other deferred value.
+
+    :returns: A value that becomes determined after both the reciever and the
+    given values become determined.
+    */
     public func both<OtherValue>(other: Deferred<OtherValue>) -> Deferred<(Value, OtherValue)> {
         return flatMap { t in other.map { u in (t, u) } }
     }
 }
 
+/**
+Compose a number of deferred values into a single deferred array.
+
+:param: deferreds Any collection whose elements are themselves deferred values.
+:return: A deferred array that is determined once all the given values are
+determined, in the same order.
+**/
 public func all<Value, Collection: CollectionType where Collection.Generator.Element == Deferred<Value>>(deferreds: Collection) -> Deferred<[Value]> {
     let array = Array(deferreds)
     if array.isEmpty {
@@ -138,6 +216,13 @@ public func all<Value, Collection: CollectionType where Collection.Generator.Ele
     return combined
 }
 
+/**
+Choose the deferred value that is determined first.
+
+:param: deferreds Any collection whose elements are themselves deferred values.
+:return: A deferred value that is determined with the first of the given
+deferred values to be determined.
+**/
 public func any<Value, Sequence: SequenceType where Sequence.Generator.Element == Deferred<Value>>(deferreds: Sequence) -> Deferred<Deferred<Value>> {
     let combined = Deferred<Deferred<Value>>()
     for d in deferreds {

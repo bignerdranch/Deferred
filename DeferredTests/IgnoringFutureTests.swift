5657de7a82d1a92943f7c78b5dc9028e91aa31dd
Zachary Waldowski <zach@waldowski.me>
2015-11-05 13:06:42 -0500

    Rename "ignoringResult" to "ignoringValue" for clarity

--- DeferredTests/IgnoringFutureTests.swift
+++ DeferredTests/IgnoringFutureTests.swift
@@ -21,7 +21,7 @@ class IgnoringFutureTests: XCTestCase {
 
     func testWaitWithTimeout() {
         let deferred = Deferred<Int>()
-        future = deferred.ignoringResult
+        future = deferred.ignoringValue
 
         let expect = expectationWithDescription("value blocks while unfilled")
         after(1, upon: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
@@ -37,7 +37,7 @@ class IgnoringFutureTests: XCTestCase {
 
     func testIgnoredUponCalledWhenFilled() {
         let d = Deferred<Int>()
-        future = d.ignoringResult
+        future = d.ignoringValue
 
         for _ in 0 ..< 10 {
             let expect = expectationWithDescription("upon blocks not called while deferred is unfilled")

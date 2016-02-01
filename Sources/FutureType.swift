6fbd5e92544bd8b32e12a785c4a632ea9a1ef80d
Zachary Waldowski <zach@waldowski.me>
2016-01-31 21:52:26 -0500

    Make all map variants return Future<T>

--- Sources/FutureType.swift
+++ Sources/FutureType.swift
@@ -124,14 +124,14 @@ public extension FutureType {
     /// - parameter transform: Start a new operation using the deferred value.
     /// - returns: The new deferred value returned by the `transform`.
     /// - seealso: Deferred
-    func flatMap<NewFuture: FutureType>(upon queue: dispatch_queue_t = genericQueue, transform: Value -> NewFuture) -> Deferred<NewFuture.Value> {
+    func flatMap<NewFuture: FutureType>(upon queue: dispatch_queue_t = genericQueue, transform: Value -> NewFuture) -> Future<NewFuture.Value> {
         let d = Deferred<NewFuture.Value>()
         upon(queue) {
             transform($0).upon(queue) {
                 d.fill($0)
             }
         }
-        return d
+        return Future(d)
     }
 
     /// Transforms the future once it becomes determined.
@@ -144,12 +144,12 @@ public extension FutureType {
     /// - parameter transform: Create something using the deferred value.
     /// - returns: A new future that is filled once the reciever is determined.
     /// - seealso: Deferred
-    func map<NewValue>(upon queue: dispatch_queue_t = genericQueue, transform: Value -> NewValue) -> Deferred<NewValue> {
+    func map<NewValue>(upon queue: dispatch_queue_t = genericQueue, transform: Value -> NewValue) -> Future<NewValue> {
         let d = Deferred<NewValue>()
         upon(queue) {
             d.fill(transform($0))
         }
-        return d
+        return Future(d)
     }
 }
 

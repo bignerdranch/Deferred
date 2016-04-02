a85f6004bc1dcf3eea92dfcab5d69ae8a9d7d9c9
Zachary Waldowski <zach@waldowski.me>
2016-04-02 14:38:37 -0400

    Add default implementations for ExecutorType

--- Sources/ExecutorType.swift
+++ Sources/ExecutorType.swift
@@ -6,8 +6,63 @@
 //  Copyright © 2014-2016 Big Nerd Ranch. All rights reserved.
 //
 
+import Foundation
+
 public protocol ExecutorType {
 
     func submit(body: () -> Void)
 
+    var underlyingQueue: dispatch_queue_t? { get }
+
 }
+
+extension ExecutorType {
+
+    public var underlyingQueue: dispatch_queue_t? {
+        return nil
+    }
+    
+}
+
+struct QueueExecutor: ExecutorType {
+
+    private let queue: dispatch_queue_t
+    init(_ queue: dispatch_queue_t) {
+        self.queue = queue
+    }
+
+    func submit(body: () -> Void) {
+        dispatch_async(queue, body)
+    }
+
+    var underlyingQueue: dispatch_queue_t? {
+        return queue
+    }
+
+}
+
+extension NSOperationQueue: ExecutorType {
+
+    public func submit(body: () -> Void) {
+        addOperationWithBlock(body)
+    }
+
+}
+
+extension CFRunLoop: ExecutorType {
+
+    public func submit(body: () -> Void) {
+        CFRunLoopPerformBlock(self, kCFRunLoopDefaultMode, body)
+        CFRunLoopWakeUp(self)
+    }
+
+}
+
+extension NSRunLoop: ExecutorType {
+
+    public func submit(body: () -> Void) {
+        getCFRunLoop().submit(body)
+    }
+
+}
+

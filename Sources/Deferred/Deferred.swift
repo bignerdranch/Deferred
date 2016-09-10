2be5a75700492439b1bf95967e3031f85fdd1984
Zachary Waldowski <zach@waldowski.me>
2016-09-10 20:39:05 +0200

    Annotate discardability of Promise.fill family

--- Sources/Deferred/Deferred.swift
+++ Sources/Deferred/Deferred.swift
@@ -125,6 +125,7 @@ public struct Deferred<Value>: FutureType, PromiseType {
     ///
     /// - parameter value: The resolved value for the instance.
     /// - returns: Whether the promise was fulfilled with `value`.
+    @discardableResult
     public func fill(_ value: Value) -> Bool {
         let wasFilled = storage.fill(value)
         if wasFilled {

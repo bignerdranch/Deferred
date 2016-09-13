12dd41f79a1f9b19275076158b0334ec159abfa3
Zachary Waldowski <zach@waldowski.me>
2016-09-13 06:22:58 +0200

    Make DispatchQueue: Executor, shorthand for upon with DispatchQueue
    Also removes the no-executor convenience for map, flatMap, and recover.

--- Sources/Deferred/FutureComposition.swift
+++ Sources/Deferred/FutureComposition.swift
@@ -6,13 +6,18 @@
 //  Copyright © 2014-2016 Big Nerd Ranch. Licensed under MIT.
 //
 
+import Dispatch
+
 extension FutureType {
     /// Returns a value that becomes determined after both the callee and the
     /// given future become determined.
     ///
     /// - seealso: SequenceType.joinedValues
     public func and<OtherFuture: FutureType>(_ other: OtherFuture) -> Future<(Value, OtherFuture.Value)> {
-        return flatMap { t in other.map { u in (t, u) } }
+        let queue = DispatchQueue.any()
+        return flatMap(upon: queue) { t in
+            other.map(upon: queue) { u in (t, u) }
+        }
     }
 
     /// Returns a value that becomes determined after the callee and both other
@@ -20,9 +25,10 @@ extension FutureType {
     ///
     /// - seealso: SequenceType.joinedValues
     public func and<Other1: FutureType, Other2: FutureType>(_ one: Other1, _ two: Other2) -> Future<(Value, Other1.Value, Other2.Value)> {
-        return flatMap { t in
-            one.flatMap { u in
-                two.map { v in (t, u, v) }
+        let queue = DispatchQueue.any()
+        return flatMap(upon: queue) { t in
+            one.flatMap(upon: queue) { u in
+                two.map(upon: queue) { v in (t, u, v) }
             }
         }
     }
@@ -32,10 +38,11 @@ extension FutureType {
     ///
     /// - seealso: SequenceType.joinedValues
     public func and<Other1: FutureType, Other2: FutureType, Other3: FutureType>(_ one: Other1, _ two: Other2, _ three: Other3) -> Future<(Value, Other1.Value, Other2.Value, Other3.Value)> {
-        return flatMap { t in
-            one.flatMap { u in
-                two.flatMap { v in
-                    three.map { w in (t, u, v, w) }
+        let queue = DispatchQueue.any()
+        return flatMap(upon: queue) { t in
+            one.flatMap(upon: queue) { u in
+                two.flatMap(upon: queue) { v in
+                    three.map(upon: queue) { w in (t, u, v, w) }
                 }
             }
         }
--- Sources/Deferred/FutureType.swift
+++ Sources/Deferred/FutureType.swift
@@ -30,13 +30,20 @@ public protocol FutureType: CustomDebugStringConvertible, CustomReflectable {
     /// A type that represents the result of some asynchronous operation.
     associatedtype Value
 
+    /// The natural executor for use with this future, either by convention or
+    /// implementation detail.
+    associatedtype PreferredExecutor: ExecutorType = DefaultExecutor
+
+    /// Calls some `body` closure once the value is determined.
+    ///
+    /// By default, calls `upon(_:body:)` with an `ExecutorType`. This method
+    /// serves as sugar for types with global members such as `DispatchQueue`.
+    func upon(_ executor: PreferredExecutor, body: @escaping(Value) -> Void)
+
     /// Call some `body` closure once the value is determined.
     ///
     /// If the value is determined, the closure should be submitted to the
     /// `executor` immediately.
-    ///
-    /// - parameter executor: A context for handling the `body` on fill.
-    /// - parameter body: A closure that uses the determined value.
     func upon(_ executor: ExecutorType, body: @escaping(Value) -> Void)
 
     /// Waits synchronously for the value to become determined.
@@ -50,41 +57,18 @@ public protocol FutureType: CustomDebugStringConvertible, CustomReflectable {
 }
 
 extension FutureType {
-    /// A generic catch-all dispatch queue for use with futures, when you just
-    /// want to throw some work into the concurrent pile. As an alternative to
-    /// the `QOS_CLASS_UTILITY` global queue, work dispatched onto this queue
-    /// on platforms with QoS will match the QoS of the caller, which is
-    /// generally the right behavior for data flow.
-    public static var genericQueue: DispatchQueue {
-        // The technique is described and used in Core Foundation:
-        // http://opensource.apple.com/source/CF/CF-1153.18/CFInternal.h
-        // https://github.com/apple/swift-corelibs-foundation/blob/master/CoreFoundation/Base.subproj/CFInternal.h#L869-L889
-        #if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
-        let qosClass = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .utility
-        return .global(qos: qosClass)
-        #else
-        return .global(qos: .utility)
-        #endif
+    public func upon(_ executor: PreferredExecutor, body: @escaping(Value) -> Void) {
+        upon(executor as ExecutorType, body: body)
     }
 }
 
-extension FutureType {
-    /// Call some `body` closure once the value is determined.
-    ///
-    /// If the value is determined, the closure will be submitted to `queue`
-    /// immediately, but this call is always asynchronous.
-    ///
-    /// - seealso: `upon(_:body:)`.
-    public func upon(_ queue: DispatchQueue, body: @escaping(Value) -> Void) {
-        upon(QueueExecutor(queue), body: body)
-    }
-
+extension FutureType where PreferredExecutor == DispatchQueue {
     /// Call some `body` closure in the background once the value is determined.
     ///
     /// If the value is determined, the closure will be enqueued immediately,
     /// but this call is always asynchronous.
     public func upon(_ body: @escaping(Value) -> Void) {
-        upon(Self.genericQueue, body: body)
+        upon(.any(), body: body)
     }
 
     /// Call some `body` closure on the main queue once the value is determined.

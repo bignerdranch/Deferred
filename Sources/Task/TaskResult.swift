ad5fd6642cbae6075734f2edfe34530535351404
Mohammad Asgharneya <masgharneya@bignerdranch.com>
2017-04-07 14:59:39 -0400

    Resolve last deprecation warning related to TaskResult

--- Sources/Task/TaskResult.swift
+++ Sources/Task/TaskResult.swift
@@ -19,7 +19,8 @@ public enum TaskResult<SuccessValue> {
 }
 #endif
 
-extension TaskResult: Either {
+#if swift(>=3.1)
+extension Task.Result: Either {
 
     public typealias Value = SuccessValue
 
@@ -56,6 +57,45 @@ extension TaskResult: Either {
         }
     }
 }
+#else
+extension TaskResult: Either {
+    
+    public typealias Value = SuccessValue
+    
+    public init(from body: () throws -> Value) {
+        do {
+            self = try .success(body())
+        } catch {
+            self = .failure(error)
+        }
+    }
+    
+    public init(failure error: Error) {
+        self = .failure(error)
+    }
+    
+    public func withValues<Return>(ifLeft left: (Error) throws -> Return, ifRight right: (Value) throws -> Return) rethrows -> Return {
+        switch self {
+        case let .success(value): return try right(value)
+        case let .failure(error): return try left(error)
+        }
+    }
+    
+    /// Create an exclusive success/failure state derived from two optionals,
+    /// in the style of Cocoa completion handlers.
+    public init(value: Value?, error: Error?) {
+        switch (value, error) {
+        case (let v?, _):
+            // Ignore error if value is non-nil
+            self = .success(v)
+        case (nil, let e?):
+            self = .failure(e)
+        case (nil, nil):
+            self = .failure(TaskResultInitializerError.invalidInput)
+        }
+    }
+}
+#endif
 
 private enum TaskResultInitializerError: Error {
     case invalidInput

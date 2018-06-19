1772b07fda7d2207ed4d3a1d61482ae45d9f9462
Zachary Waldowski <zach@waldowski.me>
2018-06-19 16:13:48 -0400

    Correctly model Collection.allSucceeded() to return [SuccessValue]

--- Sources/Task/TaskCollections.swift
+++ Sources/Task/TaskCollections.swift
@@ -15,63 +15,124 @@ import Dispatch
 import Foundation
 #endif
 
-extension Collection where Iterator.Element: FutureProtocol, Iterator.Element.Value: Either, Iterator.Element.Value.Left == Error {
-    /// Compose a number of tasks into a single notifier task.
-    ///
-    /// If any of the contained tasks fail, the returned task will be determined
-    /// with that failure. Otherwise, once all operations succeed, the returned
-    /// task will be determined a success.
-    public func allSucceeded() -> Task<Void> {
-        if isEmpty {
-            return Task(success: ())
-        }
+private struct AllFilled<SuccessValue>: FutureProtocol {
+    let group = DispatchGroup()
+    let combined = Deferred<Task<SuccessValue>.Result>()
+    #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+    let progress = Progress(parent: nil, userInfo: nil)
+    #else
+    let cancellations: [() -> Void]
+    #endif
 
-        let coalescingDeferred = Deferred<Task<Void>.Result>()
-        let group = DispatchGroup()
+    init<Base: Collection>(_ base: Base, mappingBy transform: @escaping([Base.Element]) -> SuccessValue) where Base.Element: FutureProtocol, Base.Element.Value: Either, Base.Element.Value.Left == Error {
+        let array = Array(base)
         let queue = DispatchQueue.global(qos: .utility)
 
         #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-        let progress = Progress(totalUnitCount: numericCast(count))
+        progress.totalUnitCount = numericCast(array.count)
+        #elseif swift(>=4.1)
+        self.cancellations = array.compactMap {
+            ($0 as? Task<SuccessValue>)?.cancel
+        }
         #else
-        var cancellations = [() -> Void]()
-        cancellations.reserveCapacity(numericCast(underestimatedCount))
+        self.cancellations = array.flatMap {
+            ($0 as? Task<SuccessValue>)?.cancel
+        }
         #endif
 
-        for future in self {
+        for future in array {
             #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-            if let task = future as? Task<Iterator.Element.Value.Right> {
+            if let task = future as? Task<Base.Element.Value.Right> {
                 progress.adoptChild(task.progress, orphaned: false, pendingUnitCount: 1)
             } else {
                 progress.adoptChild(.wrappingSuccess(of: future, cancellation: nil), orphaned: true, pendingUnitCount: 1)
             }
-            #else
-            if let task = future as? Task<Iterator.Element.Value.Right> {
-                cancellations.append(task.cancel)
-            }
             #endif
 
             group.enter()
-            future.upon(queue) { result in
+            future.upon(queue) { [combined, group] (result) in
                 result.withValues(ifLeft: { (error) in
-                    _ = coalescingDeferred.fill(with: .failure(error))
+                    _ = combined.fail(with: error)
                 }, ifRight: { _ in })
 
                 group.leave()
             }
         }
 
-        group.notify(queue: queue) {
-            _ = coalescingDeferred.fill(with: .success(()))
+        group.notify(queue: queue) { [combined] in
+            combined.succeed(with: transform(array))
+        }
+    }
+
+    func upon(_ executor: Executor, execute body: @escaping(Task<SuccessValue>.Result) -> Void) {
+        combined.upon(executor, execute: body)
+    }
+
+    func peek() -> Task<SuccessValue>.Result? {
+        return combined.peek()
+    }
+
+    func wait(until time: DispatchTime) -> Task<SuccessValue>.Result? {
+        return combined.wait(until: time)
+    }
+
+    #if !os(macOS) && !os(iOS) && !os(tvOS) && !os(watchOS)
+    func cancel() {
+        for cancellation in cancellations {
+            cancellation()
+        }
+    }
+    #endif
+}
+
+extension Collection where Element: FutureProtocol, Element.Value: Either, Element.Value.Left == Error {
+    /// Compose a number of tasks into a single array.
+    ///
+    /// If any of the contained tasks fail, the returned task will be determined
+    /// with that failure. Otherwise, once all operations succeed, the returned
+    /// task will be fulfilled by combining the values.
+    public func allSucceeded() -> Task<[Element.Value.Right]> {
+        guard !isEmpty else {
+            return Task(success: [])
+        }
+
+        let wrapper = AllFilled(self) { (array) -> [Element.Value.Right] in
+            #if swift(>=4.1)
+            // Expect each to be filled but not successful right now.
+            // swiftlint:disable:next force_unwrapping
+            return array.compactMap { try? $0.peek()!.extract() }
+            #else
+            // Expect each to be filled but not successful right now.
+            // swiftlint:disable:next force_unwrapping
+            return array.flatMap { try? $0.peek()!.extract() }
+            #endif
         }
 
         #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-        return Task(coalescingDeferred, progress: progress)
+        return Task(wrapper, progress: wrapper.progress)
         #else
-        return Task(coalescingDeferred) {
-            for cancellation in cancellations {
-                cancellation()
-            }
+        return Task(wrapper, cancellation: wrapper.cancel)
+        #endif
+    }
+}
+
+extension Collection where Element: FutureProtocol, Element.Value: Either, Element.Value.Left == Error, Element.Value.Right == Void {
+    /// Compose a number of tasks into a single array.
+    ///
+    /// If any of the contained tasks fail, the returned task will be determined
+    /// with that failure. Otherwise, once all operations succeed, the returned
+    /// task will be determined a success.
+    public func allSucceeded() -> Task<Void> {
+        guard !isEmpty else {
+            return Task(success: ())
         }
+
+        let wrapper = AllFilled(self) { _ in () }
+
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+        return Task(wrapper, progress: wrapper.progress)
+        #else
+        return Task(wrapper, cancellation: wrapper.cancel)
         #endif
     }
 }
